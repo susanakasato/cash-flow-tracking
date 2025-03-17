c358f935d39dee32fe3768cc90109484a5e710c9
susanakasato <susana.nakasato@gmail.com>
2025-03-17 09:21:10 -0300

    initial commit

---
+++ src/test/java/com/example/cash_flow/tracking/TrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cash_flow.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
